d5a3da787c587894eaac25477924002c3b385670
Tuan Phan The (T) <tphanthe@deloitte.ca>
2024-10-16 14:37:57 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  distDir: 'dist',
+  reactStrictMode: true,
+};
 
 export default nextConfig;
